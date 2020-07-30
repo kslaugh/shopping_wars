18e0e6591990113681a89fecd6c73e56c48fca7f
Kyle Slaugh <kyleeslaugh@gmail.com>
2020-07-30 11:15:55 -0700

    linked app with backend API part 3

--- server/routes/routes.js
+++
@@ -1,8 +0,0 @@
-const C = require('../controllers/controllers');
-module.exports = function(app){
-    app.post('/api/List', C.createList);
-    app.get('/api/Lists',C.getAllLists);
-    app.get('/api/List/:id',C.getList);
-    app.delete('/api/List/:id',C.deleteList);
-    app.put('/api/List/:id',C.updateList);
-}
