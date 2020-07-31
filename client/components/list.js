677b501f726de37ee39b14f0d82d5cad539438bd
Kyle Slaugh <kyleeslaugh@gmail.com>
2020-07-30 20:52:38 -0700

    added pull to refresh on homePAge

--- client/components/list.js
+++ client/components/list.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { ScrollView, View, Text } from 'react-native';
+import { ScrollView, View, Text, RefreshControl } from 'react-native';
 
 export default (props)=>{
     return(
-        <ScrollView style={{paddingVertical:20}}>
+        <ScrollView refreshControl={
+            <RefreshControl refreshing={props.refreshing} onRefresh={props.refresh}/>
+        }>
             {props.items.map((item,idx)=>(
                 <View style={{flex:1,justifyContent:"center",alignItems:"center",flexDirection:"row",padding:5}} key={idx}>
                     <Text onPress={()=>{props.press(item.id)}} style={{color:'white'}}>{item.name}</Text>
