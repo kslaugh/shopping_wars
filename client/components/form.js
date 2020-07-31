f4f0718cfe5f3412961a963273ed401a6aa154e0
Kyle Slaugh <kyleeslaugh@gmail.com>
2020-07-30 22:10:59 -0700

    fixed rebase with master

--- client/components/form.js
+++ client/components/form.js
@@ -1,27 +1,21 @@
 import React, { useState } from "react";
 import { StyleSheet, View, Text, TextInput, Button } from "react-native";
-import Axios from "axios";
-import { NavigationContainer } from "@react-navigation/native";
 
 export default (props)=>{
-    const [name,setName]=useState('')
-    const [quantity,setQuantity]=useState('')
-    const hClick=()=>{
-        Axios.post('http://18.223.211.4/api/items/',{'name':name,'quantity':quantity})
-        .then(props.submit)
-        .catch(e=>console.log(e))
-    }
+    const [name,setName]=useState(props.item?.name||'')
+    const [quantity,setQuantity]=useState(props.item?.quantity||'')
+    
     return(
         <View style={{flex:1,justifyContent:'space-evenly',alignItems:"center",flexDirection:"column"}}>
             <View>
                 <Text>Item Name:</Text>
-                <TextInput style={{borderWidth: 3,borderColor: 'grey', minWidth: 200, fontSize:30}} onChangeText={e=>setName(e)} autoCapitalize='sentences' value={name}></TextInput>
+                <TextInput value={name} style={{borderWidth: 3,borderColor: 'grey', minWidth: 200, fontSize:30}} onChangeText={e=>setName(e)} autoCapitalize='sentences'/>
             </View>
             <View>
                 <Text>Quantity:</Text>
-                <TextInput keyboardType='number-pad' onChangeText={e=>setQuantity(e)} style={{borderWidth: 3,borderColor: 'grey', minWidth: 200, fontSize:30}} value={quantity}></TextInput>
+                <TextInput value={quantity} keyboardType='number-pad' onChangeText={e=>setQuantity(e)} style={{borderWidth: 3,borderColor: 'grey', minWidth: 200, fontSize:30}}/>
             </View>
-            <Button color='black' onPress={hClick} title='Submit'/>
+            <Button color='black' onPress={()=>props.submit({'name':name,'quantity':quantity})} title='Submit'/>
         </View>
     )
 }
