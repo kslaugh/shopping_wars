f30d953e9fa0e508c73d43629f19859c20c75dc0
Kyle Slaugh <kyleeslaugh@gmail.com>
2020-07-30 22:20:17 -0700

    fixed create page with new form

--- client/views/create.js
+++ client/views/create.js
@@ -4,8 +4,8 @@ import { View, Text } from 'react-native';
 import Axios from 'axios';
 
 export default ({navigation})=>{
-    const hSubmit=()=>{
-        Axios.post('http://18.223.211.4/api/items/',{'name':name,'quantity':quantity})
+    const hSubmit=(e)=>{
+        Axios.post('http://18.223.211.4/api/items/',e)
         .then(()=>navigation.navigate("Home"))
         .catch(e=>console.log(e))
     }
